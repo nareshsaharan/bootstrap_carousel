c098812db7fe2e87f5918c8cb36d5b796458dfa8
Naresh Saharan <nareshsaharan2016@gmail.com>
2019-10-20 13:23:04 +0530

    first commit
    Signed-off-by: Naresh Saharan <nareshsaharan2016@gmail.com>

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/public"))
+
+app.listen(port, () => console.log(`Active on ${port} port`))
